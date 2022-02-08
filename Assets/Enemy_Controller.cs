5377d29def749bcd1e65051a5ee9839c175e203c
Shogo-Nakasaki <74720278+Shogo-Nakasaki@users.noreply.github.com>
2022-02-08 17:44:34 +0900

    main updata_005
    Add Enemy.
    敵情報の追加。

---
+++ Assets/Enemy_Controller.cs
@@ -0,0 +1,29 @@
+/**
+ *  @file   Enemy_Controller.cs
+ *  @brief  敵キャラ関連の処理を記載
+ *  @author S.Nakasaki
+ *  @date   2022.02.08
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** 
+ *  @clas   Enemy_Controller
+ *  @brief  敵キャラ関連の処理を記載
+ *          敵の情報（キャライラストや行動）の切り替えを管理
+ */
+public class Enemy_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
